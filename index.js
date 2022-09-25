8fbdf29005252f3fa45fd59080bd63aaee2556bd
ZomBoy <106455717+ZomBoyHTML@users.noreply.github.com>
2022-09-24 23:51:26 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+const si = document.querySelector('#si');
+
+si.addEventListener('click',function () {
+    alert('Avisame y vamos por las papitas (｡◕‿◕｡)♥ ')
+});
+
+const no = document.querySelector('#no');
+let a = 0;
+no.addEventListener('mouseover', function () {
+    if(a == 0){
+        document.getElementById('no').style.translate = -200 + 'px';
+        document.getElementById('si').style.translate = 200 + 'px';
+        a = 1;
+    }else if(a == 1){
+        document.getElementById('no').style.translate = 0 + 'px';
+        document.getElementById('si').style.translate = -0 + 'px';
+        a=0;
+    }
+
+    console.log(a);
+    
+})
